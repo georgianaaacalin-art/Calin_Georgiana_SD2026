60a4eaf94d7c3bb69b0051fb770fe096c1102736
Georgiana Calin <calingeorgiana24@stud.ase.ro>
2026-02-24 11:00:57 +0200

    //continuare structura lista simpla + afisare main

--- SDD/ListaSimpla.c
+++ SDD/ListaSimpla.c
@@ -19,11 +19,13 @@ alias creareFruct(const char* nume, float greutate, int nrFructe) {
 	strcpy(a.nume, nume);
 	a.greutate = greutate;
 	a.nrFructe = nrFructe;
+
+	return a;
 }
 
 typedef struct Nod nod;
 struct Nod {
-	nod info;
+	alias info;
 	struct nod* next;
 };
 
@@ -39,5 +41,61 @@ nod* inserareInceput(nod* cap, alias a) {
 	}
 
 	return cap;
+}
+
+nod* inserareSfarsit(nod* cap, alias a) {
+	nod* nou = (nod*)malloc(sizeof(nod));
+	nou->info = creareFruct(a.nume, a.greutate, a.nrFructe);
+	nou->next = NULL;
+	if (cap == NULL) {
+		cap = nou;
+	}
+	else {
+		nod* aux = cap;
+		while (aux->next) {
+			aux = aux->next;
+		}
+		aux->next = nou;
+	}
+	return cap;
+}
+
+void afisare(nod* cap) {
+	nod* aux = cap;
+	while (aux) {
+		printf("%s, %.2f, %d\n", aux->info.nume, aux->info.greutate, aux->info.nrFructe);
+		aux = aux->next;
+	}
+}
+
+void dezalocare(nod* cap) {
+	nod* aux = cap;
+	free(aux->info.nume);
+	nod* temp = aux->next;
+	free(aux);
+	aux = temp;
+}
+
+void main() {
+	alias a;
+	nod* cap = NULL;
+	char buffer[50];
+	int numar;
+
+	FILE* f = fopen("fruct.txt", "r");
+	fscanf(f, "%d", &numar);
+
+	for (int i = 0; i < numar; i++) {
+		fscanf(f, "%s", buffer);
+		a.nume = (char*)malloc(sizeof(char) * strlen(buffer) + 1);
+		strcpy(a.nume, buffer);
+
+		fscanf(f, "%.2f", &a.greutate);
+		fscanf(f, "%d", &a.nrFructe);
+	}
+
+	fclose(f);
+	afisare(cap);
+	dezalocare(cap);
 }
 
